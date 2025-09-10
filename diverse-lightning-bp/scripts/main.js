cda6e71ea2ef21403d5717348e979b0ab3c13d61
npweber <npweber@npwdev.net>
2025-09-10 16:34:11 -0700

    Test update packs

--- diverse-lightning-bp/scripts/main.js
+++ diverse-lightning-bp/scripts/main.js
@@ -1,9 +1,2 @@
-import { world, system } from "@minecraft/server";
-
 // Main script entry point for Diverse Lightning addon
 console.log("Diverse Lightning addon loaded!");
-
-// Example tick event
-system.runInterval(() => {
-    // Add your custom logic here
-}, 20); // Run every second (20 ticks)
