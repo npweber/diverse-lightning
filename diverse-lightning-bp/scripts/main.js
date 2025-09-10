042e24c5fc29ddc6ff8fe3ffade8b64d0add1b40
npweber <npweber@npwdev.net>
2025-09-10 16:20:22 -0700

    Fixed main.js

--- diverse-lightning-bp/scripts/main.js
+++ diverse-lightning-bp/scripts/main.js
@@ -3,11 +3,6 @@ import { world, system } from "@minecraft/server";
 // Main script entry point for Diverse Lightning addon
 console.log("Diverse Lightning addon loaded!");
 
-// Example event listener for world load
-world.afterEvents.worldInitialize.subscribe(() => {
-    console.log("World initialized - Diverse Lightning ready!");
-});
-
 // Example tick event
 system.runInterval(() => {
     // Add your custom logic here
