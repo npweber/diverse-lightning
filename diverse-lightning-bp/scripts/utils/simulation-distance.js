3e6a9ef6d2faa638ec9f59f7bab45a727539e31e
npweber <npweber@npwdev.net>
2025-09-12 11:42:52 -0700

    Writing simulation-distance 3

--- diverse-lightning-bp/scripts/utils/simulation-distance.js
+++ diverse-lightning-bp/scripts/utils/simulation-distance.js
@@ -2,7 +2,7 @@ import { world } from "@minecraft/server"
 
 world.afterEvents.playerSpawn.subscribe((event) => {
     const maxRenderDistance = event.player.clientSystemInfo.maxRenderDistance;
-    const playerDimension = event.player.getDimension();
+    const playerDimension = event.player.dimension;
     const playerLocation = event.player.location;
         
     let testLocationSimulated = { x: playerLocation.x + (maxRenderDistance * 16), y: playerLocation.y, z: playerLocation.z };
