96870dee7ee7373771127e23bddc7a8fb67209de
npweber <npweber@npwdev.net>
2025-09-23 13:21:24 -0700

    Writing lightning strike mechanics 13

--- diverse-lightning-bp/scripts/lightning_strikes.js
+++ diverse-lightning-bp/scripts/lightning_strikes.js
@@ -21,38 +21,38 @@ const LIGHTNING_VARIANTS = [
     }
 ]
 
-let isThunderstorm = false;
+let weatherObjective = world.scoreboard.getObjective("weather");
+if (!weatherObjective)
+    weatherObjective = world.scoreboard.addObjective("weather");
     
 system.runInterval(() => {
-    if (isThunderstorm) {
-        const SIMULATION_BOUNDING_BOX_SIDE_LENGTH = getSimulationDistance() * 2;
-        const chunksSimulated = Math.pow(SIMULATION_BOUNDING_BOX_SIDE_LENGTH, 2);
-        shouldStrikeLightningInChunks(chunksSimulated).forEach((chunkIndex) => {
-            const lightningVariantChoice = weightedRandom(LIGHTNING_VARIANTS.map(variant => variant.chance));
-            const lightningVariant = LIGHTNING_VARIANTS[lightningVariantChoice].name;
-            console.log(`${lightningVariant} strike in chunk ${chunkIndex}`);
-        });
-    }
+    world.scoreboard.getParticipants().forEach((participant) => {
+        const isThunderstorm = weatherObjective.getScore(participant);
+        if (isThunderstorm) {
+            const SIMULATION_BOUNDING_BOX_SIDE_LENGTH = getSimulationDistance(participant) * 2;
+            const chunksSimulated = Math.pow(SIMULATION_BOUNDING_BOX_SIDE_LENGTH, 2);
+            shouldStrikeLightningInChunks(chunksSimulated).forEach((chunkIndex) => {
+                const lightningVariantChoice = weightedRandom(LIGHTNING_VARIANTS.map(variant => variant.chance));
+                const lightningVariant = LIGHTNING_VARIANTS[lightningVariantChoice].name;
+                console.log(`${lightningVariant} strike in chunk ${chunkIndex}`);
+            });
+        }
+    });
 }, 1);
 
 world.afterEvents.weatherChange.subscribe((event) => {
-    if (event.newWeather === WeatherType.Thunder) 
-        isThunderstorm = true;
-    else
-        isThunderstorm = false;
+    const isThunderstorm = event.newWeather === WeatherType.Thunder ? 1 : 0;
+    world.scoreboard.getParticipants().forEach((participant) => weatherObjective.setScore(participant, isThunderstorm));
 });
 
 world.afterEvents.playerSpawn.subscribe((event) => {
-    const playerDimension = event.player.dimension;
-    if (playerDimension.id !== "minecraft:overworld") {
-        return;
+    if (!weatherObjective.hasParticipant(event.player)) {
+        weatherObjective.addParticipant(event.player);
+        weatherObjective.setScore(event.player, 0);
     }
-
-    console.log(playerDimension.runCommand("weather query").successCount);
 });
 
 function shouldStrikeLightningInChunks(chunksSimulated) {
-    console.log(`Simulating ${chunksSimulated} chunks`);
     const shouldStrikeLightningInChunks = [];
     for (let i = 0; i < chunksSimulated; i++)
         if (weightedRandom([99.999, 0.001]) === 1) 
--- diverse-lightning-bp/scripts/utils/simulation-distance.js
+++ diverse-lightning-bp/scripts/utils/simulation-distance.js
@@ -1,36 +1,28 @@
-import { world } from "@minecraft/server"
-
 const POSSIBLE_SIMULATION_DISTANCES = [12,10,8,6,4];
-let SIMULATION_DISTANCE;
 
-world.afterEvents.playerSpawn.subscribe((event) => {
-    const playerDimension = event.player.dimension;
-    if (playerDimension.id !== "minecraft:overworld") {
-        return;
-    }
-    const playerLocation = event.player.location;
-
-    for (const simulationDistance of POSSIBLE_SIMULATION_DISTANCES) {
-        try {
-            const testLocationX = playerLocation.x + (simulationDistance * 16) - 2;
-            const testLocationY = playerDimension.getTopmostBlock({x: testLocationX, z: playerLocation.z}).y + 1;
-            const testLocation = { x: testLocationX , y: testLocationY, z: playerLocation.z };
-            let testSpawnEntityResult = playerDimension.spawnEntity("minecraft:pig", testLocation);
-            if (testSpawnEntityResult !== null) {
-                SIMULATION_DISTANCE = simulationDistance;
-                testSpawnEntityResult.remove();
-                break;
+export function getSimulationDistance(player) {
+    let simulationDistance = -1;
+    const playerDimension = player.dimension;
+    if (playerDimension.id === "minecraft:overworld") {
+        const playerLocation = player.location;
+        for (const testSimulationDistance of POSSIBLE_SIMULATION_DISTANCES) {
+            try {
+                const testLocationX = playerLocation.x + (testSimulationDistance * 16) - 2;
+                const testLocationY = playerDimension.getTopmostBlock({x: testLocationX, z: playerLocation.z}).y + 1;
+                const testLocation = { x: testLocationX , y: testLocationY, z: playerLocation.z };
+                let testSpawnEntityResult = playerDimension.spawnEntity("minecraft:pig", testLocation);
+                if (testSpawnEntityResult !== null) {
+                    simulationDistance = testSimulationDistance;
+                    testSpawnEntityResult.remove();
+                    break;
+                }
+            } catch (error) {
+                if (error.name === "LocationInUnloadedChunkError") 
+                    continue;
+                else
+                    console.error(`Error spawning entity to test simulation distance "testSimulationDistance: ${testSimulationDistance}":\n ${error}`);
             }
-        } catch (error) {
-            if (error.name === "LocationInUnloadedChunkError") 
-                continue;
-            else
-                console.error(`Error spawning entity to test simulation distance "simulationDistance: ${simulationDistance}":\n ${error}`);
         }
     }
-    console.log(`Simulation distance: ${SIMULATION_DISTANCE}`);
-});
-
-export function getSimulationDistance() {
-    return SIMULATION_DISTANCE !== undefined ? SIMULATION_DISTANCE : -1;
+    return simulationDistance;
 }
